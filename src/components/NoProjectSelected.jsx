d344684e50e38078aa542ae1f9fe3c84a57846e1
soojin <kimsj0012@naver.com>
2024-05-28 20:05:27 +0900

    mod 프로젝트-추가-버튼 컴포넌트로 분리

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,4 +1,5 @@
 import noProjectImg from "../assets/no-projects.png";
+import Button from "./Button";
 
 function NoProjectSelected() {
   return (
@@ -15,7 +16,7 @@ function NoProjectSelected() {
         Select a project or get started with a new one
       </p>
       <p className="mt-8">
-        <button>Create new project</button>
+        <Button>Create new project</Button>
       </p>
     </div>
   );
