2df486ef69f2970ec32e250b671e1b834dd4c551
soojin <kimsj0012@naver.com>
2024-05-30 15:45:56 +0900

    add 모달 스타일링

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -31,10 +31,20 @@ function NewProject({ onAdd }) {
 
   return (
     <>
-      <Modal ref={modal} buttonCaption="Close">
-        <h2>Invalid Input</h2>
-        <p>Oops! Looks like you forgot to enter a value...</p>
-        <p>Please make sure you provide a valid value for every input field.</p>
+      <Modal
+        ref={modal}
+        buttonCaption="Close"
+        className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+      >
+        <h2 className="text-xl font-bold text-stone-700 mt-4 mb-4">
+          Invalid Input
+        </h2>
+        <p className="text-stone-600 mb-4">
+          Oops! Looks like you forgot to enter a value...
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex item-center justify-end gap-4 my-4">
